1bfb51b3f4f1c910e10c947d71a6f3f1c22aa98f
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2022-01-30 12:08:43 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from PIMS import __version__
+
+setup(
+    name='PIMS',
+    version=__version__,
+    packages=['PIMS'],
+    url='https://github.com/DKuzn/person-info-microservice',
+    license='MIT',
+    author='Dmitry Kuznetsov',
+    author_email='DKuznetsov2000@outlook.com',
+    description='Microservice for getting person info during face recognition',
+    install_requires=['fastapi', 'SQLAlchemy', 'numpy', 'psycopg2-binary', 'mysqlclient', 'uvicorn']
+)
